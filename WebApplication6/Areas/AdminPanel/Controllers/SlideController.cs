d7aa6555c77a9a4829f2661d1ba25691fcd4bc42
PunhanAliyeva <eliyevapunhan567@gmail.com>
2023-05-12 01:46:10 +0400

    AdminPanelCrud

--- WebApplication6/Areas/AdminPanel/Controllers/SlideController.cs
+++ WebApplication6/Areas/AdminPanel/Controllers/SlideController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 using WebApplication6.DAL;
 using WebApplication6.Models;
 
@@ -9,9 +10,11 @@ namespace WebApplication6.Areas.AdminPanel.Controllers
     public class SlideController:Controller
     {
         public AppDbContext _appDbContext;
-        public SlideController(AppDbContext appDbContext)
+        public IWebHostEnvironment _env;
+        public SlideController(AppDbContext appDbContext,IWebHostEnvironment env)
         {
             _appDbContext = appDbContext;
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
@@ -19,6 +22,7 @@ namespace WebApplication6.Areas.AdminPanel.Controllers
 
             return View(slides);
         }
+         
         public async Task<IActionResult> Create(Slide slide)
         {
             if (slide.Photo == null)
@@ -31,22 +35,69 @@ namespace WebApplication6.Areas.AdminPanel.Controllers
                 ModelState.AddModelError("Photo", "Gonderilen faylin tipi uygun deyil");
                 return View();
             }
-            FileStream file=new FileStream(@"C:\Users\ClassTime\Desktop\WebApp\WebApplication6\wwwroot\admin\images\"+slide.Photo.FileName,FileMode.Create);
-            await slide.Photo.CopyToAsync(file);
+            if (slide.Photo.Length > 200 * 1024)
+            {
+                ModelState.AddModelError("Photo", "Gonderilen faylin hecmi 200 kb-dan boyuk olmamalidir");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + slide.Photo.FileName;
+            string fullPath = Path.Combine(_env.WebRootPath, @"admin\images\BookImages",fileName);
+            using (FileStream file = new FileStream(fullPath, FileMode.Create))
+            {
+                await slide.Photo.CopyToAsync(file);
+            }
+
             slide.Image = slide.Photo.FileName;
             await _appDbContext.Slides.AddAsync(slide);
             await _appDbContext.SaveChangesAsync();
             return RedirectToAction("Index");
+            return Content(Guid.NewGuid().ToString());  
+        }
 
-        }
-        public IActionResult Update()
+
+
+
+
+        public async Task<IActionResult> Update(int? id)
         {
-            return View();
+            if (id == null || id < 1)
+            {
+                return BadRequest();
+            }
+            Slide slide = await _appDbContext.Slides.FirstOrDefaultAsync(s => s.ID == id);
+            if (slide == null)
+            {
+                return NotFound();
+            } 
+            return View(slide);
         }
-        public IActionResult Delete()
+
+
+
+
+
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if(id==null || id < 1)
+            {
+                return BadRequest();
+            }
+            Slide slide = await _appDbContext.Slides.FirstOrDefaultAsync(s => s.ID == id);
+            if (slide == null)
+            {
+                return NotFound();
+            }
+            string path = Path.Combine(_env.WebRootPath, @"admin/images/BookImages");
+            if(System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _appDbContext.Remove(slide);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
+
 
     }
+
 }
--- WebApplication6/Models/Slide.cs
+++ WebApplication6/Models/Slide.cs
@@ -5,14 +5,16 @@ namespace WebApplication6.Models
     public class Slide
     {
         public int ID { get; set; }
+        public string Image { get; set; }
         public string Title { get; set; }
         public string Subtitle { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
+
         public int Order { get; set; }
 
         [NotMapped]
         public IFormFile Photo { get; set; }
+
 
 
     }
